e33f38fd2e107bd59dee34967888c00fe1bba6fe
11m245 <sivaraj2siva@gmail.com>
2023-01-13 22:53:57 +0530

    update all commands

---
+++ zen class db task/zenClassDB_Task.js
@@ -0,0 +1,481 @@
+// show dbs
+//use zenClassDB_Task
+
+db.users.insertMany([
+  {
+    name: "Sivaraj",
+    mobile: "9500852762",
+    classes: [
+      {
+        topic: "html",
+        datedClass: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: true,
+        taskSubmittedOn: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+      },
+      {
+        topic: "css",
+        datedClass: {
+          $date: {
+            $numberLong: "1602720000000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "js",
+        datedClass: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: true,
+        taskSubmittedOn: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+      },
+      {
+        topic: "react",
+        datedClass: {
+          $date: {
+            $numberLong: "1608076800000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "node",
+        datedClass: {
+          $date: {
+            $numberLong: "1602979200000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: true,
+        taskSubmittedOn: {
+          $date: {
+            $numberLong: "1602979200000",
+          },
+        },
+      },
+    ],
+    solvedCodekata: 80,
+    mentor: "ragav",
+  },
+  {
+    name: "Ansari",
+    mobile: "9500852762",
+    classes: [
+      {
+        topic: "html",
+        datedClass: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: true,
+        taskSubmittedOn: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+      },
+      {
+        topic: "css",
+        datedClass: {
+          $date: {
+            $numberLong: "1602720000000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: true,
+        taskSubmittedOn: {
+          $date: {
+            $numberLong: "1602720000000",
+          },
+        },
+      },
+      {
+        topic: "js",
+        datedClass: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: true,
+        taskSubmittedOn: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+      },
+      {
+        topic: "node",
+        datedClass: {
+          $date: {
+            $numberLong: "1602979200000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: true,
+        taskSubmittedOn: {
+          $date: {
+            $numberLong: "1602979200000",
+          },
+        },
+      },
+    ],
+    solvedCodekata: 90,
+    mentor: "ragav",
+  },
+  {
+    name: "Settu",
+    mobile: "9500852762",
+    classes: [
+      {
+        topic: "html",
+        datedClass: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "css",
+        datedClass: {
+          $date: {
+            $numberLong: "1602720000000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "js",
+        datedClass: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "node",
+        datedClass: {
+          $date: {
+            $numberLong: "1602979200000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+    ],
+    solvedCodekata: 100,
+    mentor: "ragav",
+  },
+  {
+    name: "Srini",
+    mobile: "9500852762",
+    classes: [
+      {
+        topic: "html",
+        datedClass: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "css",
+        datedClass: {
+          $date: {
+            $numberLong: "1602720000000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "js",
+        datedClass: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "node",
+        datedClass: {
+          $date: {
+            $numberLong: "1602979200000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+    ],
+    solvedCodekata: 350,
+    mentor: "ragav",
+  },
+  {
+    name: "Jothi",
+    mobile: "9500852762",
+    classes: [
+      {
+        topic: "html",
+        datedClass: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "css",
+        datedClass: {
+          $date: {
+            $numberLong: "1602720000000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "js",
+        datedClass: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+    ],
+    solvedCodekata: 250,
+    mentor: "sanjay",
+  },
+  {
+    name: "Siva",
+    mobile: "9500852762",
+    classes: [
+      {
+        topic: "html",
+        datedClass: {
+          $date: {
+            $numberLong: "1602633600000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "css",
+        datedClass: {
+          $date: {
+            $numberLong: "1602720000000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+      {
+        topic: "js",
+        datedClass: {
+          $date: {
+            $numberLong: "1602806400000",
+          },
+        },
+        isAttended: false,
+        isTaskSubmitted: false,
+        taskSubmittedOn: "",
+      },
+    ],
+    solvedCodekata: 150,
+    mentor: "vishnu",
+  },
+]);
+
+db.mentors.insertOne({ name: "ragav", mobile: "9787191758" });
+db.mentors.insertMany([
+  { name: "sanjay", mobile: "9787191757" },
+  { name: "vishnu", mobile: "9787198571" },
+]);
+
+//Q1. Find all the topics and tasks which are thought in the month of October
+
+// since using array inside the document
+//since we want to perform opertaion on the arraylist,
+//we can unwind as individual doc and do the operation and then group
+//step 1
+db.users.aggregate([{ $unwind: { path: "$classes" } }]);
+
+//step 2
+db.users.aggregate([
+  { $unwind: { path: "$classes" } },
+  {
+    $match: {
+      $and: [
+        { "classes.datedClass": { $gte: new Date("2020-10-01") } },
+        { "classes.datedClass": { $lt: new Date("2020-11-01") } },
+      ],
+    },
+  },
+]);
+
+//step 3 - final
+db.users.aggregate([
+  { $unwind: { path: "$classes" } },
+  {
+    $match: {
+      $and: [
+        { "classes.datedClass": { $gte: new Date("2020-10-01") } },
+        { "classes.datedClass": { $lt: new Date("2020-11-01") } },
+      ],
+    },
+  },
+  { $group: { _id: "$classes.topic" } },
+]);
+
+//Q2. Find the number of problems solved by the user in codekata
+//inclusive projection=>first find then use projection value for properties:1
+db.users.find({}, { name: 1, solvedCodekata: 1 });
+
+//Q3.Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+
+// step1
+db.users.aggregate([
+  { $unwind: { path: "$classes" } },
+  {
+    $match: {
+      $and: [
+        { "classes.datedClass": { $gte: new Date("2020-10-15") } },
+        { "classes.datedClass": { $lt: new Date("2020-11-31") } },
+        { "classes.isAttended": false },
+        { "classes.isTaskSubmitted": false },
+      ],
+    },
+  },
+]);
+
+//step2
+
+db.users.aggregate([
+  { $unwind: { path: "$classes" } },
+  {
+    $match: {
+      $and: [
+        { "classes.datedClass": { $gte: new Date("2020-10-15") } },
+        { "classes.datedClass": { $lt: new Date("2020-11-31") } },
+        { "classes.isAttended": false },
+        { "classes.isTaskSubmitted": false },
+      ],
+    },
+  },
+  { $group: { _id: "$_id" } },
+]);
+//step3 => count
+db.users.aggregate([
+  { $unwind: { path: "$classes" } },
+  {
+    $match: {
+      $and: [
+        { "classes.datedClass": { $gte: new Date("2020-10-15") } },
+        { "classes.datedClass": { $lt: new Date("2020-11-31") } },
+        { "classes.isAttended": false },
+        { "classes.isTaskSubmitted": false },
+      ],
+    },
+  },
+  { $group: { _id: "$_id" } },
+  { $count: "no of absentees" },
+]);
+
+//Q4. Find all the mentors with who has the mentee's count more than 15
+// step1
+db.users.aggregate([{ $group: { _id: "$mentor", mentees: { $sum: 1 } } }]);
+
+//step2
+
+db.users.aggregate([
+  { $group: { _id: "$mentor", mentees: { $sum: 1 } } },
+  { $match: { mentees: { $gt: 15 } } },
+]);
+//step3
+//since here we dont have 15 students try with 3 mentees
+db.users.aggregate([
+  { $group: { _id: "$mentor", mentees: { $sum: 1 } } },
+  { $match: { mentees: { $gt: 3 } } },
+]);
+
+//company drives collection
+db.companyDrives.insertMany([
+  {
+    company: "TCS",
+    dated: ISODate("2020-10-13"),
+    attendedStudents: ["studentid1", "studentid2"],
+  },
+  {
+    company: "infosys",
+    dated: ISODate("2020-10-17"),
+    attendedStudents: ["studentid2", "studentid4"],
+  },
+  {
+    company: "wipro",
+    dated: ISODate("2020-10-20"),
+    attendedStudents: ["studentid5", "studentid1"],
+  },
+  {
+    company: "MindTree",
+    dated: ISODate("2020-11-13"),
+    attendedStudents: ["studentid2", "studentid7"],
+  },
+]);
+
+//Q5. Find all the company drives and students who are appeared for the placement.
+db.companyDrives.find({}, { company: 1, attendedStudents: 1 });
+
+//Q6. Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+db.companyDrives.find({
+  $and: [
+    { dated: { $gte: ISODate("2020-10-15") } },
+    { dated: { $lt: ISODate("2020-11-01") } },
+  ],
+});
